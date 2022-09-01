88e0aeb0b345f197502308da5bacca5b0d444e31
Hung Huynh <hungpchuynh@gmail.com>
2022-09-01 18:00:47 -0500

    Week1

---
+++ JavaScript/destructuring/predict_destructuring.js
@@ -0,0 +1,72 @@
+// Prolem 1:
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+
+console.log(randomCar)
+console.log(otherRandomCar)
+// Output: Testa
+// Output: Mercedes
+
+
+// Prolem 2:
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+
+console.log(name);
+console.log(otherName);
+// Output: Undefined
+// Output: Elon
+
+
+// Prolem 3:
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+
+console.log(password);
+console.log(hashedPassword);
+
+// Output: 12345
+// Output: Undefined
+
+
+// Prolem 4:
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+
+console.log(first == second);
+console.log(first == third);
+
+// Output: False
+// Output: True
+
+
+// Prolem 5:
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// Output: value
+// Output: [1, 5, 1, 8, 3, 3]
+// Output: 1
+// Output: 5

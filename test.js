af05c325fb18c5e91e3983e524b932cf80e7ab88
Hung Huynh <hungpchuynh@gmail.com>
2022-09-02 12:42:22 -0500

    week1

---
+++ test.js
@@ -0,0 +1,18 @@
+class Deck {
+    constructor() {
+        const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+        const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+        const deck = [];
+        suits.forEach(suit => {
+            faces.forEach(face => {
+                deck.push(this.createCard(suit, face));
+            });
+            });
+            this.deck = deck;
+        }
+        createCard(suit, face) {
+            return face + " of " + suit;
+        }
+    }
+
+createCard();

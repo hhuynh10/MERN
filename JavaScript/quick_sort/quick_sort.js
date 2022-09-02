86b42a91b5600c0abf5db6e742c5f4dae85fe4af
Hung Huynh <hungpchuynh@gmail.com>
2022-09-02 14:55:03 -0500

    week1

--- JavaScript/quick_sort/quick_sort.js
+++ JavaScript/quick_sort/quick_sort.js
@@ -1,10 +1,10 @@
 var items = [1,5,3,7,6,8,12,2,9];
-function swap(items, leftIndex, rightIndex){
+const swap = (items, leftIndex, rightIndex) => {
     var temp = items[leftIndex];
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
-function partition(items, left, right) {
+const partition = (items, left, right)  => {
     var pivot   = items[Math.floor((right + left) / 2)],
         i       = left;
         j       = right; 
@@ -24,8 +24,8 @@ function partition(items, left, right) {
     return i;
 }
 
-function quickSort(items, left, right) {
-    var index;
+const quickSort = (items, left, right) => {
+    let index;
     if (items.length > 1) {
         index = partition(items, left, right); 
         if (left < index - 1) {
@@ -38,5 +38,5 @@ function quickSort(items, left, right) {
     return items;
 }
 
-var sortedArray = quickSort(items, 0, items.length - 1);
+let sortedArray = quickSort(items, 0, items.length - 1);
 console.log(sortedArray);

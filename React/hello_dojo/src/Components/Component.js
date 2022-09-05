7c597babc55de46bf98783e58834ffd2ada60852
Hung Huynh <hungpchuynh@gmail.com>
2022-09-05 16:41:27 -0500

    week1

---
+++ React/hello_dojo/src/Components/Component.js
@@ -0,0 +1,17 @@
+
+const Component = () => {
+    return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <p>Thing I need to do:</p>
+            <ul>
+                <li>Learn react</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Component;

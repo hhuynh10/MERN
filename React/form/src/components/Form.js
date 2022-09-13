4374a319ddf1c5c24b2978fcf34d434f85f162d6
Hung Huynh <hungpchuynh@gmail.com>
2022-09-13 16:26:43 -0500

    week2

--- React/form/src/components/Form.js
+++ React/form/src/components/Form.js
@@ -12,12 +12,29 @@ const Form = () => {
         e.preventDefault();
         
         const newUser = { firstName, lastName, email, password, confirmPassword };
-        console.log("Welcome", newUser);
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPassword("");
-        setConfirmPassword("");
+        if (firstName && lastName && email && password){
+            if (firstName.length > 3) {
+                console.log("Welcome", newUser);
+                setFirstName("");
+                setLastName("");
+                setEmail("");
+                setPassword("");
+                setConfirmPassword("");
+            } else {
+                setFirstName("");
+                setLastName("");
+                setEmail("");
+                setPassword("");
+                setConfirmPassword("");
+            }
+        } else {
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
+        }
+        
     };
 
     return (
@@ -25,23 +42,32 @@ const Form = () => {
             <div>
                 <label>First Name: </label> 
                 <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) } />
+                {firstName.length < 3 ? 
+                <p>First name must be at least 3 characters!</p> : null}
             </div>
             <div>
                 <label>Last Name: </label> 
                 <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) } />
+                {lastName.length < 3 ? 
+                <p>Last name must be at least 3 characters!</p> : null}
             </div>
             <div>
                 <label>Email Address: </label> 
                 <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                {email.length < 3 ? 
+                <p>Email must be at least 3 characters!</p> : null}
             </div>
             <div>
                 <label>Password: </label>
                 <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                {password.length < 3 ? 
+                <p>Password must be at least 3 characters!</p> : null}
             </div>
             <div>
                 <label>Comfirm Password: </label>
                 <input type="password" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
             </div>
+            <input type="submit" value="Create User" />
     </form>
     );
 };

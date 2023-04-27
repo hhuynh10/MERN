d4ce08fd864efabac585f6cb93258ffb0656459d
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-04-26 22:16:36 -0500

    mern

--- React/webdevsimplify/React-Simplified-Projects/75-76-basic-blog-project/before/client/src/pages/Post.jsx
+++ React/webdevsimplify/React-Simplified-Projects/75-76-basic-blog-project/before/client/src/pages/Post.jsx
@@ -3,44 +3,51 @@ import { Link, useLoaderData } from "react-router-dom";
 import axios from "axios";
 
 const Post = () => {
-  const post = useLoaderData();
-  const comments = useLoaderData();
+  const { post, comments, user } = useLoaderData();
 
   console.log(comments);
 
   return (
     <>
       <h1 className="page-title">{post.title}</h1>
+      <span className="page-subtitle">
+        By: <Link to={`/users/${post.userId}`}>{user.name}</Link>
+      </span>
       <div>{post.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
       <div className="card-stack">
         <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">{comments.email}</div>
-            laudantium enim quasi est quidem magnam voluptate ipsam eos tempora
-            quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente
-            accusantium
-          </div>
+          {comments.map((comment) => {
+            return (
+              <div className="card-body" key={comment.id}>
+                <div className="text-sm mb-1">{comment.email}</div>
+                {comment.body}
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
   );
 };
 
-const loader = ({ request: { signal }, params }) => {
-  return axios
+async function loader({ request: { signal }, params }) {
+  const post = await axios
     .get(`http://127.0.0.1:3000/posts/${params.postId}`, { signal })
     .then((res) => res.data);
-};
 
-const loader1 = ({ request: { signal }, params }) => {
-  return axios
+  const comments = axios
     .get(`http://127.0.0.1:3000/posts/${params.postId}/comments`, { signal })
     .then((res) => res.data);
-};
+
+  const user = axios
+    .get(`http://127.0.0.1:3000/users/${post.userId}`, { signal })
+    .then((res) => res.data);
+
+  return { post, comments: await comments, user: await user };
+}
 
 export const postRoute = {
   loader,
-  loader1,
   element: <Post />,
 };
--- React/webdevsimplify/React-Simplified-Projects/75-76-basic-blog-project/before/client/src/pages/User.jsx
+++ React/webdevsimplify/React-Simplified-Projects/75-76-basic-blog-project/before/client/src/pages/User.jsx
@@ -3,7 +3,9 @@ import { Link, useLoaderData } from "react-router-dom";
 import axios from "axios";
 
 const User = () => {
-  const user = useLoaderData();
+  const { user, todos, posts } = useLoaderData();
+
+  console.log(user);
   return (
     <>
       <h1 className="page-title">{user.name}</h1>
@@ -18,15 +20,39 @@ const User = () => {
         <b>Address:</b> {user.address.street} {user.address.suite},{" "}
         {user.address.city}, {user.address.zipcode}
       </div>
+
+      <h3 className="mt-4 mb-2">Posts</h3>
+
+      <div className="card">
+        <div className="card-header">{posts.title}</div>
+        <div className="card-body">
+          <div className="card-preview-text">{posts.body}</div>
+        </div>
+      </div>
+
+      <h3 className="mt-4 mb-2">Todos</h3>
+      <ul>
+        <li>{todos.title}</li>
+      </ul>
     </>
   );
 };
 
-const loader = ({ request: { signal }, params }) => {
-  return axios
+async function loader({ request: { signal }, params }) {
+  const posts = axios
+    .get(`http://127.0.0.1:3000/posts/${params.userId}`, { signal })
+    .then((res) => res.data);
+
+  const todos = axios
+    .get(`http://127.0.0.1:3000/todos/${params.userId}`, { signal })
+    .then((res) => res.data);
+
+  const user = axios
     .get(`http://127.0.0.1:3000/users/${params.userId}`, { signal })
     .then((res) => res.data);
-};
+
+  return { user: await user, todos: await todos, posts: await posts };
+}
 
 export const userRoute = {
   loader,
